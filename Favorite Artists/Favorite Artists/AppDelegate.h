4d93bfc53c2d9ab82a09d5d222f95588a260e3d6
Bronson <bronsonmullens@icloud.com>
2020-09-25 18:17:53 -0700

    Set up storyboard

---
+++ Favorite Artists/Favorite Artists/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Favorite Artists
+//
+//  Created by Bronson Mullens on 9/25/20.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Favorite Artists/Favorite Artists/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  Favorite Artists
+//
+//  Created by Bronson Mullens on 9/25/20.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
